0ed29fad0f4f6e576a27a7f29b368b99939b44e2
Illia Zolotukha <illia@luday.se>
2023-09-04 15:12:44 +0300

    CREATE: expo app with tailwind, nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./components/**/*.js", "./screens/**/*.js", "./App.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
