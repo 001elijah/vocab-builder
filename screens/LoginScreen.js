c32c7c8270c7063b603fb2e724d391034dfc6259
Illia Zolotukha <illia@luday.se>
2023-09-26 10:35:16 +0300

    ADD: formik yup login/register validation

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,17 +7,33 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
 import React, { useState } from "react";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import EyeIcon from "../assets/icons/EyeIcon";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/auth/authOperations";
 
+const loginValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Please enter valid email")
+    .required("Email Address is Required"),
+  password: yup
+    .string()
+    .matches(
+      /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/,
+      "Must Contain 6 Characters, One Number"
+    )
+    .min(7, ({ min }) => `Password must be ${min} symbols`)
+    .max(7, ({ max }) => `Password must be ${max} symbols`)
+    .required("Password is required"),
+});
+
 const LoginScreen = ({ navigation }) => {
   const dispatch = useDispatch();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(true);
 
   const [isEmailInFocus, setIsEmailInFocus] = useState(false);
@@ -28,13 +44,12 @@ const LoginScreen = ({ navigation }) => {
 
   const toggleShowPassword = () => setShowPassword(!showPassword);
 
-  const handleLoginSubmit = async () => {
+  const handleLoginSubmit = async (values) => {
     Keyboard.dismiss();
     const userData = {
-      email: email,
-      password: password,
+      email: values.email,
+      password: values.password,
     };
-
     dispatch(login(userData));
   };
 
@@ -63,48 +78,103 @@ const LoginScreen = ({ navigation }) => {
             <Text className="mt-4 text-base text-black font-['FixelDisplay-Regular']">
               Please enter your login details to continue using our service:
             </Text>
-            <TextInput
-              className={`h-14 w-full my-4 px-5 align-middle border ${
-                isEmailInFocus ? "border-green" : "border-light-grey"
-              } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
-              value={email}
-              onChangeText={setEmail}
-              onFocus={toggleEmailFocus}
-              onBlur={toggleEmailFocus}
-              placeholder="Email"
-              placeholderTextColor="#121417"
-              blurOnSubmit={true}
-            />
-            <View className="w-full mb-8">
-              <TextInput
-                className={`h-14 pl-5 pr-10 border ${
-                  isPasswordInFocus ? "border-green" : "border-light-grey"
-                } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
-                value={password}
-                onChangeText={setPassword}
-                onFocus={togglePsswordFocus}
-                onBlur={togglePsswordFocus}
-                placeholder="Password"
-                placeholderTextColor="#121417"
-                blurOnSubmit={true}
-                secureTextEntry={showPassword}
-              />
-              <TouchableOpacity
-                className="absolute items-center justify-center h-12 w-9 top-1 right-1 rounded-2xl"
-                activeOpacity={0.5}
-                onPress={toggleShowPassword}
-              >
-                <EyeIcon />
-              </TouchableOpacity>
-            </View>
-            <TouchableOpacity
-              className="flex-2 items-center justify-center w-full h-14 bg-green rounded-[30px]"
-              onPress={handleLoginSubmit}
+            <Formik
+              validationSchema={loginValidationSchema}
+              initialValues={{ email: "", password: "" }}
+              onSubmit={(values) => handleLoginSubmit(values)}
             >
-              <Text className="font-['FixelDisplay-Bold'] text-base text-white">
-                Login
-              </Text>
-            </TouchableOpacity>
+              {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                values,
+                touched,
+                errors,
+                isValid,
+              }) => (
+                <>
+                  <View>
+                    <TextInput
+                      className={`h-14 w-full my-4 px-5 align-middle border ${
+                        isEmailInFocus ? "border-green" : "border-light-grey"
+                      } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
+                      value={values.email}
+                      onChangeText={handleChange("email")}
+                      onFocus={toggleEmailFocus}
+                      onEndEditing={handleBlur("email")}
+                      onBlur={toggleEmailFocus}
+                      placeholder="Email"
+                      placeholderTextColor="#121417"
+                      blurOnSubmit={true}
+                    />
+                    {errors.email && touched.email && (
+                      <Text
+                        style={{
+                          position: "absolute",
+                          bottom: 20,
+                          left: 21,
+                          fontSize: 10,
+                          color: "red",
+                        }}
+                      >
+                        {errors.email}
+                      </Text>
+                    )}
+                  </View>
+                  <View className="w-full mb-8">
+                    <TextInput
+                      className={`h-14 pl-5 pr-10 border ${
+                        isPasswordInFocus ? "border-green" : "border-light-grey"
+                      } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
+                      value={values.password}
+                      onChangeText={handleChange("password")}
+                      onFocus={togglePsswordFocus}
+                      onEndEditing={handleBlur("password")}
+                      onBlur={togglePsswordFocus}
+                      placeholder="Password"
+                      placeholderTextColor="#121417"
+                      blurOnSubmit={true}
+                      secureTextEntry={showPassword}
+                    />
+                    {errors.password && touched.password && (
+                      <Text
+                        style={{
+                          position: "absolute",
+                          bottom: 5,
+                          left: 21,
+                          fontSize: 10,
+                          color: "red",
+                        }}
+                      >
+                        {errors.password}
+                      </Text>
+                    )}
+                    <TouchableOpacity
+                      className="absolute items-center justify-center h-12 w-9 top-1 right-1 rounded-2xl"
+                      activeOpacity={0.5}
+                      onPress={toggleShowPassword}
+                    >
+                      <EyeIcon />
+                    </TouchableOpacity>
+                  </View>
+                  <TouchableOpacity
+                    className={`flex-2 items-center justify-center w-full h-14 bg-${
+                      isValid &&
+                      values.email.trim().length !== 0 &&
+                      values.password.trim().length !== 0
+                        ? "green"
+                        : "greenish"
+                    } rounded-[30px]`}
+                    onPress={handleSubmit}
+                    disabled={!isValid}
+                  >
+                    <Text className="font-['FixelDisplay-Bold'] text-base text-white">
+                      Login
+                    </Text>
+                  </TouchableOpacity>
+                </>
+              )}
+            </Formik>
             <TouchableOpacity
               className="mx-auto bg-transparent mt-4"
               onPress={() => navigation.navigate("RegistrationScreen")}
--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -7,18 +7,34 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
 import React, { useState } from "react";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import EyeIcon from "../assets/icons/EyeIcon";
 import { useDispatch } from "react-redux";
 import { register } from "../redux/auth/authOperations";
 
+const registrationValidationSchema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup
+    .string()
+    .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Please enter valid email")
+    .required("Email Address is Required"),
+  password: yup
+    .string()
+    .matches(
+      /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/,
+      "Must Contain 6 Characters, One Number"
+    )
+    .min(7, ({ min }) => `Password must be ${min} symbols`)
+    .max(7, ({ max }) => `Password must be ${max} symbols`)
+    .required("Password is required"),
+});
+
 const RegistrationScreen = ({ navigation }) => {
   const dispatch = useDispatch();
 
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(true);
 
   const [isNameInFocus, setIsNameInFocus] = useState(false);
@@ -31,14 +47,14 @@ const RegistrationScreen = ({ navigation }) => {
 
   const toggleShowPassword = () => setShowPassword(!showPassword);
 
-  const handleRegistrationSubmit = async () => {
+  const handleRegistrationSubmit = async (values) => {
     Keyboard.dismiss();
     const newUserData = {
-      name: fullName,
-      email: email,
-      password: password,
+      name: values.name,
+      email: values.email,
+      password: values.password,
     };
-
+    
     dispatch(register(newUserData));
   };
 
@@ -65,60 +81,132 @@ const RegistrationScreen = ({ navigation }) => {
               To start using our services, please fill out the registration form
               below. All fields are mandatory:
             </Text>
-            <TextInput
-              className={`h-14 w-full my-4 px-5 border ${
-                isNameInFocus ? "border-green" : "border-light-grey"
-              } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
-              value={fullName}
-              onChangeText={setFullName}
-              onFocus={toggleNameFocus}
-              onBlur={toggleNameFocus}
-              placeholder="Name"
-              placeholderTextColor="#121417"
-              blurOnSubmit={true}
-            />
-            <TextInput
-              className={`h-14 w-full mb-4 px-5 border ${
-                isEmailInFocus ? "border-green" : "border-light-grey"
-              } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
-              value={email}
-              onChangeText={setEmail}
-              onFocus={toggleEmailFocus}
-              onBlur={toggleEmailFocus}
-              placeholder="Email"
-              placeholderTextColor="#121417"
-              blurOnSubmit={true}
-            />
-            <View className="w-full mb-8">
-              <TextInput
-                className={`h-14 pl-5 pr-10 border ${
-                  isPasswordInFocus ? "border-green" : "border-light-grey"
-                } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
-                value={password}
-                onChangeText={setPassword}
-                onFocus={togglePsswordFocus}
-                onBlur={togglePsswordFocus}
-                placeholder="Password"
-                placeholderTextColor="#121417"
-                blurOnSubmit={true}
-                secureTextEntry={showPassword}
-              />
-              <TouchableOpacity
-                className="absolute items-center justify-center h-12 w-9 top-1 right-1 rounded-2xl"
-                activeOpacity={0.5}
-                onPress={toggleShowPassword}
-              >
-                <EyeIcon />
-              </TouchableOpacity>
-            </View>
-            <TouchableOpacity
-              className="flex-2 items-center justify-center w-full h-14 bg-green rounded-[30px]"
-              onPress={handleRegistrationSubmit}
+            <Formik
+              validationSchema={registrationValidationSchema}
+              initialValues={{ name: "", email: "", password: "" }}
+              onSubmit={(values) => handleRegistrationSubmit(values)}
             >
-              <Text className="font-['FixelDisplay-Bold'] text-base text-white">
-                Register
-              </Text>
-            </TouchableOpacity>
+              {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                values,
+                touched,
+                errors,
+                isValid,
+              }) => (
+                <>
+                  <View>
+                    <TextInput
+                      className={`h-14 w-full my-4 px-5 border ${
+                        isNameInFocus ? "border-green" : "border-light-grey"
+                      } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
+                      value={values.name}
+                      onChangeText={handleChange("name")}
+                      onFocus={toggleNameFocus}
+                      onEndEditing={handleBlur("name")}
+                      onBlur={toggleNameFocus}
+                      placeholder="Name"
+                      placeholderTextColor="#121417"
+                      blurOnSubmit={true}
+                    />
+                    {errors.name && touched.name && (
+                      <Text
+                        style={{
+                          position: "absolute",
+                          bottom: 20,
+                          left: 21,
+                          fontSize: 10,
+                          color: "red",
+                        }}
+                      >
+                        {errors.name}
+                      </Text>
+                    )}
+                  </View>
+                  <View>
+                    <TextInput
+                      className={`h-14 w-full mb-4 px-5 border ${
+                        isEmailInFocus ? "border-green" : "border-light-grey"
+                      } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
+                      value={values.email}
+                      onChangeText={handleChange("email")}
+                      onFocus={toggleEmailFocus}
+                      onEndEditing={handleBlur("email")}
+                      onBlur={toggleEmailFocus}
+                      placeholder="Email"
+                      placeholderTextColor="#121417"
+                      blurOnSubmit={true}
+                    />
+                    {errors.email && touched.email && (
+                      <Text
+                        style={{
+                          position: "absolute",
+                          bottom: 21,
+                          left: 21,
+                          fontSize: 10,
+                          color: "red",
+                        }}
+                      >
+                        {errors.email}
+                      </Text>
+                    )}
+                  </View>
+                  <View className="w-full mb-8">
+                    <TextInput
+                      className={`h-14 pl-5 pr-10 border ${
+                        isPasswordInFocus ? "border-green" : "border-light-grey"
+                      } rounded-2xl font-['FixelDisplay-Regular'] text-base text-black`}
+                      value={values.password}
+                      onChangeText={handleChange("password")}
+                      onFocus={togglePsswordFocus}
+                      onEndEditing={handleBlur("password")}
+                      onBlur={togglePsswordFocus}
+                      placeholder="Password"
+                      placeholderTextColor="#121417"
+                      blurOnSubmit={true}
+                      secureTextEntry={showPassword}
+                    />
+                    <TouchableOpacity
+                      className="absolute items-center justify-center h-12 w-9 top-1 right-1 rounded-2xl"
+                      activeOpacity={0.5}
+                      onPress={toggleShowPassword}
+                    >
+                      <EyeIcon />
+                    </TouchableOpacity>
+                    {errors.password && touched.password && (
+                      <Text
+                        style={{
+                          position: "absolute",
+                          bottom: 5,
+                          left: 21,
+                          fontSize: 10,
+                          color: "red",
+                        }}
+                      >
+                        {errors.password}
+                      </Text>
+                    )}
+                  </View>
+                  <TouchableOpacity
+                    className={`flex-2 items-center justify-center w-full h-14 bg-${
+                      isValid &&
+                      values.name.trim().length !== 0 &&
+                      values.email.trim().length !== 0 &&
+                      values.password.trim().length !== 0
+                        ? "green"
+                        : "greenish"
+                    } rounded-[30px]`}
+                    onPress={handleSubmit}
+                    disabled={!isValid}
+                  >
+                    <Text className="font-['FixelDisplay-Bold'] text-base text-white">
+                      Register
+                    </Text>
+                  </TouchableOpacity>
+                </>
+              )}
+            </Formik>
             <TouchableOpacity
               className="mx-auto bg-transparent mt-4"
               onPress={() => navigation.navigate("LoginScreen")}
